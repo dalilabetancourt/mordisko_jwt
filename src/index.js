b9bc7462b35107856e5b363085dbd05c9ea3beea
¨Dalila <dalilabetancourt50@gmail.com>
2026-04-01 20:50:52 -0300

    :tada: servidor corriendo

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import exphbs from 'express-handlebars';
+import path from 'path';  
+
+const __dirname = path.resolve();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//static public
+app.use(express.static(path.join(__dirname, 'src/public'))) 
+
+
+//sincronizar con la base de datos
+
+
+//rutas 
+app.set('views engine', 'hbs')
+app.set('views', path.join(__dirname, 'src/views'));
+
+app.engine('hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'src/views/layouts'),
+    extname: '.hbs'
+}))
+
+//rutas
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
