b62e0538104726e6230e47d7587178f8d8116934
¨Dalila <dalilabetancourt50@gmail.com>
2026-04-02 01:14:16 -0300

    :tada:creacion de ruta categoria

--- src/controllers/mordiskoController.js
+++ src/controllers/mordiskoController.js
@@ -2,8 +2,19 @@
 
 const home = (req, res) => {
     try {
+      const products = [
+            {
+                id: 1,
+                nombre: 'Helado de chocolate',
+                descripcion: 'Cremoso y delicioso',
+                precio: 3000,
+                categoria: 'Leche'
+            }
+        ]
         res.render('home', {
-            pageTitle: 'inicio'
+            pageTitle: 'inicio',
+            isExito: true,
+            products
         })
     } catch (error) {
         console.log('Error al cargar pagina home', error)
